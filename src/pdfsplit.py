4a3654a1507c50d045c46901b7557d8952d6e54a
Christopher Schmidt <christopher.schmidt@posteo.de>
2018-12-23 19:04:07 +0100

    Switch to relative parameters

--- src/pdfsplit.py
+++ src/pdfsplit.py
@@ -2,11 +2,11 @@ import PyPDF2
 
 
 def get_page(page, writer, upper_left, lower_right, index):
-    dimensions = page.artBox
-    new_page = writer.insertBlankPage(dimensions[2], dimensions[3], index)
+    _, _, dim_x, dim_y = page.artBox
+    new_page = writer.insertBlankPage(dim_x, dim_y, index)
     new_page.mergePage(page)
-    new_page.cropBox.upperLeft = upper_left
-    new_page.cropBox.lowerRight = lower_right
+    new_page.cropBox.upperLeft = upper_left[0] * dim_x, upper_left[1] * dim_y
+    new_page.cropBox.lowerRight = lower_right[0] * dim_x, lower_right[1] * dim_y
 
 
 def main(input_path, output_path, crops):
@@ -27,4 +27,4 @@ def main(input_path, output_path, crops):
 
 if __name__ == '__main__':
     main("../TNN_WS18_01_Intro_slides.pdf", 'results.pdf',
-         [((0, 595), (421, 298)), ((0, 298), (421, 0)), ((421, 595), (842, 298)), ((421, 298), (842, 0))])
+         [((0, 1), (0.5, 0.5)), ((0, 0.5), (0.5, 0)), ((0.5, 1), (1, 0.5)), ((0.5, 0.5), (1, 0))])
